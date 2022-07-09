1bfb40e0f9e962fba20ab573c7d07389263d43f2
= <malyo20.daiki@gmail.com>
2022-07-09 21:27:08 +0300

    ready for edit component

--- src/components/edit-product.component.js
+++ src/components/edit-product.component.js
@@ -4,6 +4,40 @@ import { useParams } from "react-router";
 
 
 const EditProduct = () => {
+	const {id} = useParams();
+	const axiosProvider = require("axios").create({
+        timeout: 120000, // 2 min
+        baseURL:'http://localhost:5000'
+    });
+	const [product, setProduct] = useState(
+        {
+            article: "",
+            couleur: "",
+            image: "",
+            marque: "",
+            qualite: "",
+            taille: "",
+            prix: "",
+            type: "",
+        }
+    );
+	useEffect(() => {
+        axiosProvider({
+            method: "GET",
+            url: "/produits/"+id,
+            headers: { 'Access-Control-Allow-Origin': true }
+        }).then((res) => {
+            let data = {}
+            Object.entries(res.data).forEach(([key, value]) => {
+                console.log(key, value)
+                data = Object.assign({},data,{[key]:value})
+            })
+			
+			setProduct(data)
+			console.log('list product',product)
+        })
+   },[])
+
 	return(
 		<div className="product-edit">
 			<h2>Edit here</h2>
